364c018fd089c7056c8a0257f8b22251cb6e0cad
Mustafa Yılmaz <mustafa.yilmaz@aartimuhendislik.com>
2024-01-17 14:10:17 +0300

    Project added

---
+++ Simple Bank.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "country_code" int
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial,
+  "from_account_id" bigin NOT NULL,
+  "to_account_id" bigin NOT NULL,
+  "code" int,
+  "name" varchar,
+  "continent_name" varchar,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  PRIMARY KEY ("id", "code")
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
