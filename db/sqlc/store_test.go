201ea8c371ef6615f9c904a37a5d7388690af231
Mustafa Yılmaz <mustafa.yilmaz@aartimuhendislik.com>
2024-01-23 16:33:46 +0300

    deadlock solved, transition handled

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -19,8 +19,10 @@ func TestTransferTx(t *testing.T) {
 	errs := make(chan error)
 	results := make(chan TransferTxResults)
 	for i := 0; i < n; i++ {
+
 		go func() {
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.Background()
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
